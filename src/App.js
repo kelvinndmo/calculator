695cbd21547a339e96055027e89a535971c4521b
kelvin onkundi <kelvinonkundi@kelvins-MacBook-Pro.local>
2020-02-13 22:10:41 +0300

    work on calculator logic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,176 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    previousText: "",
+    currentText: "",
+    currentValue: "",
+    previousValue: "",
+    operation: ""
+  };
+
+  appendNumber = e => {
+    let number = e.target.value;
+    const { currentValue } = this.state;
+    if (number === "." && currentValue.includes(".")) return;
+    this.setState({
+      currentValue: currentValue.toString() + number.toString()
+    });
+  };
+
+  chooseOperation = e => {
+    const { currentValue, previousValue } = this.state;
+    let operation = e.target.value;
+    if (currentValue === "") return;
+    if (previousValue !== "") {
+      this.calculate();
+    }
+
+    this.setState({ operation });
+    this.setState({ previousValue: currentValue });
+    this.setState({ currentValue: "" });
+  };
+
+  delete = () => {
+    const { currentValue } = this.state;
+    const newValue = currentValue.toString().slice(0, -1);
+    this.setState({ currentValue: newValue });
+  };
+
+  clear = () => {
+    this.setState({ currentValue: "" });
+    this.setState({ previousValue: "" });
+    this.setState({ operation: "" });
+  };
+
+  updateDisplay = () => {
+    const { previousValue, operation } = this.state;
+    if (operation !== null) {
+      this.setState({ previousText: previousValue });
+    }
+  };
+
+  getDisplayNumber = number => {
+    const stringNumber = number.toString();
+    const intergerDit = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+
+    let IntergerValue;
+    if (isNaN(intergerDit)) {
+      IntergerValue = "";
+    } else {
+      IntergerValue = intergerDit.toLocaleString("en", {
+        maximumFractionDigits: 0
+      });
+    }
+
+    if (decimalDigits != null) {
+      return `${IntergerValue}.${decimalDigits}`;
+    } else {
+      return IntergerValue;
+    }
+  };
+
+  calculate = () => {
+    const { operation, previousValue, currentValue } = this.state;
+    let computation;
+    const previous = parseFloat(previousValue);
+    const current = parseFloat(currentValue);
+
+    if (isNaN(previous) || isNaN(current)) return;
+    switch (operation) {
+      case "+":
+        computation = previous + current;
+        break;
+      case "-":
+        computation = previous - current;
+        break;
+      case "*":
+        computation = previous * current;
+        break;
+      case "/":
+        computation = previous / current;
+        break;
+      default:
+        computation = "";
+    }
+    this.setState({ currentValue: computation });
+    this.setState({ operation: "" });
+    this.setState({ previousValue: "" });
+  };
+
+  equalsSign = () => {
+    this.calculate();
+    this.updateDisplay();
+  };
+
+  render() {
+    const { currentValue, previousValue, operation } = this.state;
+    return (
+      <div className="calculator-grid">
+        <div className="output">
+          <div className="previous-operand">{`${this.getDisplayNumber(
+            previousValue
+          )} ${operation}`}</div>
+          <div className="current-operand">{currentValue}</div>
+        </div>
+        <button onClick={this.clear} className="span-two">
+          AC
+        </button>
+        <button onClick={this.delete}>DEL</button>
+        <button onClick={this.chooseOperation} value="/">
+          /
+        </button>
+        <button value={1} onClick={this.appendNumber}>
+          1
+        </button>
+        <button value={2} onClick={this.appendNumber}>
+          2
+        </button>
+        <button value={3} onClick={this.appendNumber}>
+          3
+        </button>
+        <button onClick={this.chooseOperation} value={"*"}>
+          *
+        </button>
+        <button value={4} onClick={this.appendNumber}>
+          4
+        </button>
+        <button value={5} onClick={this.appendNumber}>
+          5
+        </button>
+        <button value={6} onClick={this.appendNumber}>
+          6
+        </button>
+        <button onClick={this.chooseOperation} value={"+"}>
+          +
+        </button>
+        <button value={7} onClick={this.appendNumber}>
+          7
+        </button>
+        <button value={8} onClick={this.appendNumber}>
+          8
+        </button>
+        <button value={9} onClick={this.appendNumber}>
+          9
+        </button>
+        <button onClick={this.chooseOperation} value={"-"}>
+          -
+        </button>
+        <button value={"."} onClick={this.appendNumber}>
+          .
+        </button>
+        <button value={0} onClick={this.appendNumber}>
+          0
+        </button>
+        <button onClick={this.equalsSign} className="span-two">
+          =
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,230 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(<App />);
+  wrapper
+    .find("button")
+    .at(4)
+    .simulate("click", {
+      target: { value: "2" }
+    });
+});
+
+test("should render the component App", () => {
+  expect(wrapper).toMatchSnapshot();
+});
+
+test("should append a number when clicked", () => {
+  expect(wrapper.state("currentValue")).toBe("2");
+});
+
+test("should append operator when operator is clicked", () => {
+  wrapper
+    .find("button")
+    .at(2)
+    .simulate("click", {
+      target: { value: "/" }
+    });
+  expect(wrapper.state("operation")).toBe("/");
+});
+
+test("should call calculate when equal sign is presses", () => {
+  //we called with wrapper with one in the beforeEach funtion let's call
+  // another sign operator then ccall calculate to calculate the result by
+  // simulating the equals button
+  wrapper
+    .find("button")
+    .at(2)
+    .simulate("click", {
+      target: { value: "/" }
+    });
+
+  wrapper
+    .find("button")
+    .at(3)
+    .simulate("click", {
+      target: { value: "1" }
+    });
+
+  //lets now simulate the = button
+  wrapper
+    .find("button")
+    .at(17)
+    .simulate("click", {});
+
+  expect(wrapper.state("currentValue")).toBe(2);
+});
+
+test("should multiply when a multiplication sign is clicked", () => {
+  wrapper
+    .find("button")
+    .at(6)
+    .simulate("click", {
+      target: { value: "*" }
+    });
+
+  wrapper
+    .find("button")
+    .at(4)
+    .simulate("click", {
+      target: { value: "2" }
+    });
+
+  //lets now simulate the = button
+  wrapper
+    .find("button")
+    .at(17)
+    .simulate("click", {});
+  //expect 2*2 to be 4
+  expect(wrapper.state("currentValue")).toBe(4);
+});
+
+test("should add when a addition sign is clicked", () => {
+  wrapper
+    .find("button")
+    .at(10)
+    .simulate("click", {
+      target: { value: "+" }
+    });
+
+  wrapper
+    .find("button")
+    .at(4)
+    .simulate("click", {
+      target: { value: "2" }
+    });
+
+  //lets now simulate the = button
+  wrapper
+    .find("button")
+    .at(17)
+    .simulate("click", {});
+  //expect 2+2 to be 4
+  expect(wrapper.state("currentValue")).toBe(4);
+});
+
+test("should substract when a substraction sign is clicked", () => {
+  wrapper
+    .find("button")
+    .at(14)
+    .simulate("click", {
+      target: { value: "-" }
+    });
+
+  wrapper
+    .find("button")
+    .at(4)
+    .simulate("click", {
+      target: { value: "2" }
+    });
+
+  //lets now simulate the = button
+  wrapper
+    .find("button")
+    .at(17)
+    .simulate("click", {});
+  //expect 2-2 to be 0
+  expect(wrapper.state("currentValue")).toBe(0);
+});
+
+test("should test that the input field is cleared", () => {
+  //simulate the clear button
+  wrapper
+    .find("button")
+    .at(0)
+    .simulate("click", {});
+  expect(wrapper.state("currentValue")).toBe("");
+  expect(wrapper.state("previousValue")).toBe("");
+  expect(wrapper.state("operation")).toBe("");
+});
+
+test("should delete when delete button is clicked", () => {
+  wrapper
+    .find("button")
+    .at(1)
+    .simulate("click", {});
+  expect(wrapper.state("currentValue")).toBe("");
+  expect(wrapper.state("previousValue")).toBe("");
+  expect(wrapper.state("operation")).toBe("");
+});
+
+test("should automatically calculate when more than one operation is passed", () => {
+  wrapper
+    .find("button")
+    .at(14)
+    .simulate("click", {
+      target: { value: "-" }
+    });
+
+  wrapper
+    .find("button")
+    .at(4)
+    .simulate("click", {
+      target: { value: "2" }
+    });
+  wrapper
+    .find("button")
+    .at(14)
+    .simulate("click", {
+      target: { value: "-" }
+    });
+  wrapper
+    .find("button")
+    .at(4)
+    .simulate("click", {
+      target: { value: "2" }
+    });
+  expect(wrapper.state("currentValue")).toBe("2");
+});
+
+test("should previous text should be empty when an operator has not been input", () => {
+  expect(wrapper.state("previousText")).toBe("");
+});
+
+test("should add decimals when a addition sign is clicked with dot character", () => {
+  wrapper
+    .find("button")
+    .at(10)
+    .simulate("click", {
+      target: { value: "+" }
+    });
+
+  wrapper
+    .find("button")
+    .at(4)
+    .simulate("click", {
+      target: { value: "2" }
+    });
+  wrapper
+    .find("button")
+    .at(15)
+    .simulate("click", {
+      target: { value: "." }
+    });
+  wrapper
+    .find("button")
+    .at(4)
+    .simulate("click", {
+      target: { value: "2" }
+    });
+
+  //lets now simulate the = button
+  wrapper
+    .find("button")
+    .at(17)
+    .simulate("click", {});
+  //expect 2+2 to be 4
+  expect(wrapper.state("currentValue")).toBe(4.2);
+});
+
+test("should return when switch has no operation", () => {
+  wrapper
+    .find("button")
+    .at(17)
+    .simulate("click", {});
+  expect(wrapper.state("operation")).toBe("");
 });
